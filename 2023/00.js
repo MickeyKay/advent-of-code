791e63f10d325bd205abe7ab33185547d0de0d1b
MickeyKay <mickeyskay@gmail.com>
2023-12-02 08:16:51 -0800

    Add template and complete 02

---
+++ 2023/00.js
@@ -0,0 +1,30 @@
+/**
+ * NOTES
+ */
+
+/**
+ * SETUP & HELPERS
+ */
+
+const TEST_INPUT = `
+
+`;
+
+const REAL_INPUT = `
+
+`;
+
+// Get array of parsed lines.
+const getParsedInputLines = input => {
+  return input.split('\n').filter(n => n);
+}
+
+/**
+ * PART ONE
+ */
+
+
+/**
+ * PART TWO
+ */
+
